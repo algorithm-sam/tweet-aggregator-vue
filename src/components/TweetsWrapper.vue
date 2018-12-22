b1b3562648a836a3e05fc7a933a423c0594fe0e3
unknown <oluwatobisamuel2020@gmail.com>
2018-12-22 18:04:15 +0000

    Made the tweets draggable and sortable although there is still a bug

--- src/components/TweetsWrapper.vue
+++ src/components/TweetsWrapper.vue
@@ -3,36 +3,55 @@
     <b-loading v-if="loading" :is-full-page="false" :active.sync="loading"></b-loading>
     <div class="container">
       <!-- <div class="columns mag-top"> -->
-        <draggable v-model="children" class="columns mag-top">
-          <tweet-container
-            class="column p-10 is-primary tweets"
-            v-for="(child,index) in children"
-            :key="index"
-            :tweet="tweets[index]"
-            :numInCol="settings.numTweets || 30"
-          />
-        </draggable>
+      <draggable v-model="tweetsData" class="columns mag-top" @end="onEnd">
+        <tweet-container
+          class="column p-10 is-primary tweets"
+          v-for="(child,index) in tweetsData"
+          :key="index"
+          :tweet="tweets[index]"
+          :numInCol="settings.numTweets || 30"
+        />
+      </draggable>
       <!-- </div> -->
     </div>
   </div>
 </template>
 
 <script>
-import draggable from 'vuedraggable';
+import draggable from "vuedraggable";
 import TweetContainer from "./TweetContainer";
 export default {
   name: "tweetCon",
   props: ["tweets", "loading", "settings"],
   components: {
-    TweetContainer, draggable
+    TweetContainer,
+    draggable
   },
   data() {
     return {
       children: 3
     };
   },
-
-  methods: {}
+  computed: {
+    tweetsData: {
+      get() {
+        return this.tweets;
+      },
+      set(value) {
+        this.tweets = value;
+      }
+    }
+  },
+  methods: {
+    onEnd: function(arg) {
+      // console.log(arg.oldIndex, arg.newIndex);
+      let oldIndex = arg.oldIndex;
+      let newIndex = arg.newIndex;
+      let oldValue = tweetsData[oldIndex];
+      this.tweets[oldIndex] = this.tweets[newIndex];
+      this.tweets.newIndex = oldValue;
+    }
+  }
 };
 </script>
 
@@ -40,10 +59,9 @@ export default {
 .mag-top {
   margin-top: 50px;
 }
-.p-10{
+.p-10 {
   padding: 5px;
   margin: 5px;
   /*background-color: red*/
 }
-
 </style>
