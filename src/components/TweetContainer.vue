a64553699a6cc3e39f0501cdf63b97f0596e233f
unknown <oluwatobisamuel2020@gmail.com>
2018-12-22 18:03:12 +0000

    made the tweet containers scrollable

--- src/components/TweetContainer.vue
+++ src/components/TweetContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="scrollable">
     <Tweet v-for="(child,index) in tweet" :key="index" :tweet="child"></Tweet>
   </div>
 </template>
@@ -23,4 +23,30 @@ export default {
 </script>
 
 <style>
+.scrollable {
+  height: 100vh;
+  max-height: 100vh;
+  overflow: hidden;
+}
+
+.scrollable:hover {
+  overflow: scroll;
+}
+
+.scrollable::-webkit-scrollbar,
+.scrollable::-webkit-scrollbar-thumb {
+  overflow: visible;
+  border-radius: 4px;
+}
+
+.scrollable::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.2);
+  opacity: 0;
+}
+.scrollable:hover .scrollable::-webkit-scrollbar-thumb {
+  opacity: 1;
+}
+.scrollable::-webkit-scrollbar {
+  width: 0.4em;
+}
 </style>
